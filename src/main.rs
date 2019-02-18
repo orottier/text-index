9cb474d0c29c5d7f0e7352d49e43a7b48a6eb23c
Otto <otto@ot-to.nl>
2019-02-18 18:35:23 +0100

    Store length of csv record so we dont need any parsing when filtering

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use multimap::MultiMap;
 use std::error::Error;
 
 use std::fs::File;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
 use std::io::Write;
 
 fn main() -> Result<(), Box<Error>> {
@@ -65,15 +68,34 @@ fn main() -> Result<(), Box<Error>> {
     unreachable!();
 }
 
-fn index(file: File, column: usize) -> Result<MultiMap<String, u64>, Box<Error>> {
-    let mut index = MultiMap::<String, u64>::new();
+fn index(file: File, column: usize) -> Result<MultiMap<String, (u64, u64)>, Box<Error>> {
+    let mut index = MultiMap::<String, (u64, u64)>::new();
     let mut rdr = csv::Reader::from_reader(file);
 
-    for result in rdr.records() {
-        let record = result?;
-        let pos = record.position().unwrap().byte();
-        index.insert(record[column].to_owned(), pos);
-        println!("{} @ {}", record[column].to_owned(), pos);
+    let mut record = csv::StringRecord::new();
+    let mut prev_value = String::from("");
+    let mut prev_pos = 0;
+    let mut pos;
+
+    while rdr.read_record(&mut record)? {
+        pos = record.position().unwrap().byte();
+        println!("read   {} @ {}", record[column].to_owned(), pos);
+
+        if prev_pos != 0 {
+            println!("insert {} @ {}", prev_value, pos - prev_pos);
+            index.insert(prev_value.clone(), (prev_pos, pos - prev_pos));
+        }
+
+        // no new alloc
+        prev_value.clear();
+        prev_value.push_str(&record[column]);
+
+        prev_pos = pos;
+    }
+    if prev_pos != 0 {
+        pos = record.position().unwrap().byte();
+        println!("insert {} @ {}", prev_value, pos - prev_pos);
+        index.insert(prev_value, (prev_pos, pos - prev_pos));
     }
 
     println!("{:?}", index.get_vec("1"));
@@ -81,24 +103,23 @@ fn index(file: File, column: usize) -> Result<MultiMap<String, u64>, Box<Error>>
     Ok(index)
 }
 
-fn filter(file: File, filename: &str, column: usize, value: &str) -> Result<(), Box<Error>> {
-    let mut rdr = csv::Reader::from_reader(file);
-
+fn filter(mut file: File, filename: &str, column: usize, value: &str) -> Result<(), Box<Error>> {
     let fh = File::open(format!("{}.index.{}", filename, column + 1))?;
-    let index: MultiMap<String, u64> = bincode::deserialize_from(fh)?;
+    let index: MultiMap<String, (u64, u64)> = bincode::deserialize_from(fh)?;
 
-    let mut record = csv::StringRecord::new();
     index
         .get_vec(value)
         .unwrap_or(&vec![])
         .into_iter()
-        .for_each(|&byte_pos| {
-            let mut pos = csv::Position::new();
-            pos.set_byte(byte_pos);
-            rdr.seek(pos).unwrap();
-            assert!(rdr.read_record(&mut record).unwrap()); // must be true unless underlying file has changed since index
+        .for_each(|&(byte_pos, len)| {
+            let mut buf = vec![0u8; len as usize];
+            file.seek(SeekFrom::Start(byte_pos))
+                .expect("Unable to seek file pos");
+            file.read_exact(&mut buf).expect("Unable to read file");
 
-            println!("{:?}", record);
+            // may result in invalid utf8 if file has changed after index
+            let record = unsafe { std::str::from_utf8_unchecked(&buf) };
+            print!("{}", record);
         });
 
     Ok(())
