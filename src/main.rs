11432b6bdfc6ad936fbc68c02a298098a9d1c47d
Otto <otto@ot-to.nl>
2019-02-20 21:48:04 +0100

    Use BTreeMap for index so we can perform ranged queries

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 extern crate bincode;
 extern crate clap;
 extern crate csv;
-extern crate multimap;
 
 use clap::{value_t, App, Arg, SubCommand};
 
-use multimap::MultiMap;
 use std::error::Error;
+use std::ops::Bound::{Excluded, Included, Unbounded};
 
 use std::fs::File;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::Write;
+
+use std::collections::BTreeMap;
+
+type CsvIndex = BTreeMap<String, Vec<(u64, u64)>>;
+
+enum Operation {
+    EQ,
+    LT,
+    LE,
+    GT,
+    GE,
+}
+
+struct Filter<'a> {
+    op: Operation,
+    value: &'a str,
+    column: usize,
+}
 
 fn main() -> Result<(), Box<Error>> {
     let matches = App::new("csv_index")
@@ -32,7 +49,8 @@ fn main() -> Result<(), Box<Error>> {
             SubCommand::with_name("filter")
                 .help("Filter records on a column value")
                 .arg(Arg::with_name("COLUMN").required(true).index(1))
-                .arg(Arg::with_name("VALUE").required(true).index(2)),
+                .arg(Arg::with_name("OP").required(true).index(2))
+                .arg(Arg::with_name("VALUE").required(true).index(3)),
         )
         .get_matches();
 
@@ -62,14 +80,25 @@ fn main() -> Result<(), Box<Error>> {
             .value_of("VALUE")
             .expect("required arg cannot be None");
 
-        return filter(file, &filename, column, value);
+        let op_str = matches.value_of("OP").expect("required arg cannot be None");
+        let op = match op_str.to_uppercase().as_ref() {
+            "LT" => Operation::LT,
+            "LE" => Operation::LE,
+            "EQ" => Operation::EQ,
+            "GE" => Operation::GE,
+            "GT" => Operation::GT,
+            _ => panic!("Unknown operator"),
+        };
+        let select = Filter { op, value, column };
+
+        return filter(file, &filename, &select);
     }
 
     unreachable!();
 }
 
-fn index(file: File, column: usize) -> Result<MultiMap<String, (u64, u64)>, Box<Error>> {
-    let mut index = MultiMap::<String, (u64, u64)>::new();
+fn index(file: File, column: usize) -> Result<CsvIndex, Box<Error>> {
+    let mut index = CsvIndex::new();
     let mut rdr = csv::Reader::from_reader(file);
 
     let mut record = csv::StringRecord::new();
@@ -79,11 +108,12 @@ fn index(file: File, column: usize) -> Result<MultiMap<String, (u64, u64)>, Box<
 
     while rdr.read_record(&mut record)? {
         pos = record.position().unwrap().byte();
-        println!("read   {} @ {}", record[column].to_owned(), pos);
 
         if prev_pos != 0 {
-            println!("insert {} @ {}", prev_value, pos - prev_pos);
-            index.insert(prev_value.clone(), (prev_pos, pos - prev_pos));
+            index
+                .entry(prev_value.clone())
+                .or_insert_with(|| vec![])
+                .push((prev_pos, pos - prev_pos));
         }
 
         // no new alloc
@@ -94,23 +124,35 @@ fn index(file: File, column: usize) -> Result<MultiMap<String, (u64, u64)>, Box<
     }
     if prev_pos != 0 {
         pos = record.position().unwrap().byte();
-        println!("insert {} @ {}", prev_value, pos - prev_pos);
-        index.insert(prev_value, (prev_pos, pos - prev_pos));
+        index
+            .entry(prev_value)
+            .or_insert_with(|| vec![])
+            .push((prev_pos, pos - prev_pos));
     }
 
-    println!("{:?}", index.get_vec("1"));
+    println!("Read {} rows", index.len());
 
     Ok(index)
 }
 
-fn filter(mut file: File, filename: &str, column: usize, value: &str) -> Result<(), Box<Error>> {
-    let fh = File::open(format!("{}.index.{}", filename, column + 1))?;
-    let index: MultiMap<String, (u64, u64)> = bincode::deserialize_from(fh)?;
+fn filter(mut file: File, filename: &str, select: &Filter) -> Result<(), Box<Error>> {
+    let fh = File::open(format!("{}.index.{}", filename, select.column + 1))?;
+    let index: CsvIndex = bincode::deserialize_from(fh)?;
+
+    let bounds = match select.op {
+        Operation::EQ => (
+            Included(select.value.to_owned()),
+            Included(select.value.to_owned()),
+        ),
+        Operation::LE => (Unbounded, Included(select.value.to_owned())),
+        Operation::LT => (Unbounded, Excluded(select.value.to_owned())),
+        Operation::GT => (Excluded(select.value.to_owned()), Unbounded),
+        Operation::GE => (Included(select.value.to_owned()), Unbounded),
+    };
 
     index
-        .get_vec(value)
-        .unwrap_or(&vec![])
-        .into_iter()
+        .range(bounds)
+        .flat_map(|(_key, vals)| vals.into_iter())
         .for_each(|&(byte_pos, len)| {
             let mut buf = vec![0u8; len as usize];
             file.seek(SeekFrom::Start(byte_pos))
