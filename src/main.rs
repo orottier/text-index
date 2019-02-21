6df5a4e5437390ea21bf76a5767119c4a07cba12
Otto <otto@ot-to.nl>
2019-02-21 22:04:29 +0100

    Columns can be indexed as string, int or float

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 extern crate bincode;
 extern crate clap;
 extern crate csv;
+extern crate serde;
+
+mod unsafe_float;
+use crate::unsafe_float::UnsafeFloat;
 
 use clap::{value_t, App, Arg, SubCommand};
+use serde::{Deserialize, Serialize};
 
 use std::error::Error;
 use std::ops::Bound::{Excluded, Included, Unbounded};
@@ -18,7 +23,44 @@ use flate2::Compression;
 
 use std::collections::BTreeMap;
 
-type CsvIndex = BTreeMap<String, Vec<(u64, u64)>>;
+use std::f64;
+use std::i64;
+
+type CsvIndex<R> = BTreeMap<R, Vec<(u64, u64)>>;
+
+#[derive(Serialize, Deserialize)]
+enum CsvIndexType {
+    STR(CsvIndex<String>),
+    I64(CsvIndex<i64>),
+    F64(CsvIndex<UnsafeFloat>),
+}
+
+#[inline]
+fn insert_csv_index(index: &mut CsvIndexType, key: String, value: (u64, u64)) -> () {
+    match index {
+        CsvIndexType::STR(index) => index.entry(key).or_insert_with(|| vec![]).push(value),
+        CsvIndexType::I64(index) => {
+            let key = key.parse().unwrap_or(i64::MIN);
+            index.entry(key).or_insert_with(|| vec![]).push(value)
+        }
+        CsvIndexType::F64(index) => {
+            let key = UnsafeFloat(key.parse().unwrap_or(f64::NEG_INFINITY));
+            index.entry(key).or_insert_with(|| vec![]).push(value)
+        }
+    }
+}
+
+#[inline]
+fn print_record(file: &mut File, byte_pos: u64, len: u64) {
+    let mut buf = vec![0u8; len as usize];
+    file.seek(SeekFrom::Start(byte_pos))
+        .expect("Unable to seek file pos");
+    file.read_exact(&mut buf).expect("Unable to read file");
+
+    // may result in invalid utf8 if file has changed after index
+    let record = unsafe { std::str::from_utf8_unchecked(&buf) };
+    print!("{}", record);
+}
 
 enum Operation {
     EQ,
@@ -46,7 +88,8 @@ fn main() -> Result<(), Box<Error>> {
         .subcommand(
             SubCommand::with_name("index")
                 .help("Build an index for a given column")
-                .arg(Arg::with_name("COLUMN").required(true).index(1)),
+                .arg(Arg::with_name("COLUMN").required(true).index(1))
+                .arg(Arg::with_name("TYPE").required(false).index(2)),
         )
         .subcommand(
             SubCommand::with_name("filter")
@@ -67,7 +110,9 @@ fn main() -> Result<(), Box<Error>> {
         let column = value_t!(matches.value_of("COLUMN"), usize).unwrap_or_else(|e| e.exit());
         let column = column - 1; // index starts at 1
 
-        let index = index(file, column)?;
+        let csv_type = matches.value_of("TYPE").unwrap_or("STR").to_uppercase();
+
+        let index = index(file, column, csv_type)?;
 
         let fh = File::create(format!("{}.index.{}", filename, column + 1))?;
         let gz = GzEncoder::new(fh, Compression::fast());
@@ -101,23 +146,27 @@ fn main() -> Result<(), Box<Error>> {
     unreachable!();
 }
 
-fn index(file: File, column: usize) -> Result<CsvIndex, Box<Error>> {
-    let mut index = CsvIndex::new();
-    let mut rdr = csv::Reader::from_reader(file);
+fn index(file: File, column: usize, csv_type: String) -> Result<CsvIndexType, Box<Error>> {
+    let mut index = match csv_type.as_ref() {
+        "STR" => CsvIndexType::STR(CsvIndex::<String>::new()),
+        "INT" => CsvIndexType::I64(CsvIndex::<i64>::new()),
+        "FLOAT" => CsvIndexType::F64(CsvIndex::<UnsafeFloat>::new()),
+        _ => panic!("Unknown operator"),
+    };
 
+    let mut rdr = csv::Reader::from_reader(file);
     let mut record = csv::StringRecord::new();
     let mut prev_value = String::from("");
     let mut prev_pos = 0;
     let mut pos;
+    let mut counter = 0u64;
 
     while rdr.read_record(&mut record)? {
         pos = record.position().unwrap().byte();
 
         if prev_pos != 0 {
-            index
-                .entry(prev_value.clone())
-                .or_insert_with(|| vec![])
-                .push((prev_pos, pos - prev_pos));
+            counter += 1;
+            insert_csv_index(&mut index, prev_value.clone(), (prev_pos, pos - prev_pos));
         }
 
         // no new alloc
@@ -127,14 +176,17 @@ fn index(file: File, column: usize) -> Result<CsvIndex, Box<Error>> {
         prev_pos = pos;
     }
     if prev_pos != 0 {
+        counter += 1;
         pos = record.position().unwrap().byte();
-        index
-            .entry(prev_value)
-            .or_insert_with(|| vec![])
-            .push((prev_pos, pos - prev_pos));
+        insert_csv_index(&mut index, prev_value, (prev_pos, pos - prev_pos));
     }
 
-    println!("Read {} rows", index.len());
+    let unique = match &index {
+        CsvIndexType::STR(index) => index.len(),
+        CsvIndexType::I64(index) => index.len(),
+        CsvIndexType::F64(index) => index.len(),
+    };
+    println!("Read {} rows with {} unique values", counter, unique);
 
     Ok(index)
 }
@@ -142,32 +194,65 @@ fn index(file: File, column: usize) -> Result<CsvIndex, Box<Error>> {
 fn filter(mut file: File, filename: &str, select: &Filter) -> Result<(), Box<Error>> {
     let fh = File::open(format!("{}.index.{}", filename, select.column + 1))?;
     let gz = GzDecoder::new(fh);
-    let index: CsvIndex = bincode::deserialize_from(gz)?;
+    let index: CsvIndexType = bincode::deserialize_from(gz)?;
 
-    let bounds = match select.op {
-        Operation::EQ => (
-            Included(select.value.to_owned()),
-            Included(select.value.to_owned()),
-        ),
-        Operation::LE => (Unbounded, Included(select.value.to_owned())),
-        Operation::LT => (Unbounded, Excluded(select.value.to_owned())),
-        Operation::GT => (Excluded(select.value.to_owned()), Unbounded),
-        Operation::GE => (Included(select.value.to_owned()), Unbounded),
+    match index {
+        CsvIndexType::STR(typed_index) => {
+            let bounds = match select.op {
+                Operation::EQ => (
+                    Included(select.value.to_owned()),
+                    Included(select.value.to_owned()),
+                ),
+                Operation::LE => (Unbounded, Included(select.value.to_owned())),
+                Operation::LT => (Unbounded, Excluded(select.value.to_owned())),
+                Operation::GT => (Excluded(select.value.to_owned()), Unbounded),
+                Operation::GE => (Included(select.value.to_owned()), Unbounded),
+            };
+
+            typed_index
+                .range(bounds)
+                .flat_map(|(_key, vals)| vals.into_iter())
+                .for_each(|&(byte_pos, len)| {
+                    print_record(&mut file, byte_pos, len);
+                });
+        }
+        CsvIndexType::I64(typed_index) => {
+            let value: i64 = select.value.parse().unwrap_or(i64::MIN);
+            let bounds = match select.op {
+                Operation::EQ => (Included(value), Included(value)),
+                Operation::LE => (Excluded(i64::MIN), Included(value)),
+                Operation::LT => (Excluded(i64::MIN), Excluded(value)),
+                Operation::GT => (Excluded(value), Excluded(i64::MAX)),
+                Operation::GE => (Included(value), Excluded(i64::MAX)),
+            };
+
+            typed_index
+                .range(bounds)
+                .flat_map(|(_key, vals)| vals.into_iter())
+                .for_each(|&(byte_pos, len)| {
+                    print_record(&mut file, byte_pos, len);
+                });
+        }
+        CsvIndexType::F64(typed_index) => {
+            let value: UnsafeFloat = UnsafeFloat(select.value.parse().unwrap_or(f64::NEG_INFINITY));
+            let lower = Excluded(UnsafeFloat(f64::NEG_INFINITY));
+            let upper = Excluded(UnsafeFloat(f64::INFINITY));
+            let bounds = match select.op {
+                Operation::EQ => (Included(value), Included(value)),
+                Operation::LE => (lower, Included(value)),
+                Operation::LT => (lower, Excluded(value)),
+                Operation::GT => (Excluded(value), upper),
+                Operation::GE => (Included(value), upper),
+            };
+
+            typed_index
+                .range(bounds)
+                .flat_map(|(_key, vals)| vals.into_iter())
+                .for_each(|&(byte_pos, len)| {
+                    print_record(&mut file, byte_pos, len);
+                });
+        }
     };
-
-    index
-        .range(bounds)
-        .flat_map(|(_key, vals)| vals.into_iter())
-        .for_each(|&(byte_pos, len)| {
-            let mut buf = vec![0u8; len as usize];
-            file.seek(SeekFrom::Start(byte_pos))
-                .expect("Unable to seek file pos");
-            file.read_exact(&mut buf).expect("Unable to read file");
-
-            // may result in invalid utf8 if file has changed after index
-            let record = unsafe { std::str::from_utf8_unchecked(&buf) };
-            print!("{}", record);
-        });
 
     Ok(())
 }
---
+++ src/unsafe_float.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub struct UnsafeFloat(pub f64);
+
+impl Eq for UnsafeFloat {}
+impl Ord for UnsafeFloat {
+    fn cmp(&self, other: &UnsafeFloat) -> Ordering {
+        self.0.partial_cmp(&other.0).unwrap_or(Ordering::Less)
+    }
+}
+impl PartialOrd for UnsafeFloat {
+    fn partial_cmp(&self, other: &UnsafeFloat) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl PartialEq for UnsafeFloat {
+    fn eq(&self, other: &UnsafeFloat) -> bool {
+        self.0 == other.0
+    }
+}
